c16aab4bb5eb51eb1a99fcaaf305a12e59b5b193
Dave Gurnell <dave@untyped.com>
2016-08-15 10:42:29 +0100

    Draft rewrite of UpdateRule.

--- src/main/scala/checklist/update/UpdateRule.scala
+++ src/main/scala/checklist/update/UpdateRule.scala
@@ -9,35 +9,32 @@ import monocle.{PLens, Lens}
 import scala.language.higherKinds
 import Message.{errors, warnings}
 
-sealed abstract class UpdateRule[A, B, C] extends Serializable {
-  def apply(original: A, update: B): Checked[C]
+sealed abstract class UpdateRule[A, B] {
+  def apply(original: A, updated: A): Checked[B]
 
-  def map[D](func: C => D): UpdateRule[A, B, D] =
-    UpdateRule.pure[A, B, D]((original, update) => this(original, update) map func)
+  def map[C](func: B => C): UpdateRule[A, C] =
+    UpdateRule.pure[A, C]((original, updated) => this(original, updated) map func)
 
-  def flatMap[D](func: C => UpdateRule[A, B, D]): UpdateRule[A, B, D] =
-    UpdateRule.pure[A, B, D]((original, update) => this(original, update) flatMap (func(_)(original, update)))
+  def flatMap[C](func: B => UpdateRule[A, C]): UpdateRule[A, C] =
+    UpdateRule.pure[A, C]((original, updated) => this(original, updated) flatMap (func(_)(original, updated)))
 
-  def andThen[D](that: UpdateRule[C, B, D]): UpdateRule[A, B, D] =
-    UpdateRule.pure[A, B, D]((original, update) => this(original, update) flatMap (that.apply(_, update)))
-
-  def zip[D](that: UpdateRule[A, B, D]): UpdateRule[A, B, (C, D)] =
-    UpdateRule.pure[A, B, (C, D)] { (original, update) =>
-      this(original, update) match {
+  def zip[C](that: UpdateRule[A, C]): UpdateRule[A, (B, C)] =
+    UpdateRule.pure[A, (B, C)] { (original, updated) =>
+      this(original, updated) match {
         case Ior.Left(msg1) =>
-          that(original, update) match {
+          that(original, updated) match {
             case Ior.Left(msg2)    => Ior.left(msg1 concat msg2)
             case Ior.Right(c)      => Ior.left(msg1)
             case Ior.Both(msg2, c) => Ior.left(msg1 concat msg2)
           }
         case Ior.Right(b) =>
-          that(original, update) match {
+          that(original, updated) match {
             case Ior.Left(msg2)    => Ior.left(msg2)
             case Ior.Right(c)      => Ior.right((b, c))
             case Ior.Both(msg2, c) => Ior.both(msg2, (b, c))
           }
         case Ior.Both(msg1, b) =>
-          that(original, update) match {
+          that(original, updated) match {
             case Ior.Left(msg2)    => Ior.left(msg1 concat msg2)
             case Ior.Right(c)      => Ior.both(msg1, (b, c))
             case Ior.Both(msg2, c) => Ior.both(msg1 concat msg2, (b, c))
@@ -46,15 +43,15 @@ sealed abstract class UpdateRule[A, B, C] extends Serializable {
     }
 
   // TODO: Write unit tests. And be prepared. There's a high chance this is all horribly wrong.
-  def prefix[P: PathPrefix](prefix: P): UpdateRule[A, B, C] =
-    UpdateRule.pure((original, update) => this(original, update) leftMap (_ map (_ prefix prefix)))
+  def prefix[P: PathPrefix](prefix: P): UpdateRule[A, B] =
+    UpdateRule.pure((original, updated) => this(original, updated) leftMap (_ map (_ prefix prefix)))
 
   // TODO: Write unit tests. And be prepared. There's a high chance this is all horribly wrong.
-  def composeLens[S, T](lens: PLens[S, T, A, C]): UpdateRule[S, B, T] =
-    UpdateRule.pure((original, update) => this(lens.get(original), update) map (lens.set(_)(original)))
+  def composeLens[S, T](lens: PLens[S, T, A, B]): UpdateRule[S, T] =
+    UpdateRule.pure((original, updated) => this(lens.get(original), lens.get(updated)) map (lens.set(_)(original)))
 
   // TODO: Write unit tests. And be prepared. There's a high chance this is all horribly wrong.
-  def at[P: PathPrefix, S, T](prefix: P, lens: PLens[S, T, A, C]): UpdateRule[S, B, T] =
+  def at[P: PathPrefix, S, T](prefix: P, lens: PLens[S, T, A, B]): UpdateRule[S, T] =
     this composeLens lens prefix prefix
 }
 
@@ -63,27 +60,27 @@ object UpdateRule extends BaseUpdateRules
   with UpdateRule1Syntax
 
 trait BaseUpdateRules {
-  def pure[A, B, C](func: (A, B) => Checked[C]): UpdateRule[A, B, C] =
-    new UpdateRule[A, B, C] {
-      def apply(original: A, update: B) =
-        func(original, update)
+  def pure[A, B](func: (A, A) => Checked[B]): UpdateRule[A, B] =
+    new UpdateRule[A, B] {
+      def apply(original: A, updated: A) =
+        func(original, updated)
     }
 }
 
 trait PropertyUpdateRules {
-  def alwaysReplace[A, B >: A]: UpdateRule[A, B, B] =
-    UpdateRule.pure[A, B, B]((original, update) => Ior.right(update))
+  def alwaysReplace[A, B >: A]: UpdateRule[A, B] =
+    UpdateRule.pure[A, B]((original, updated) => Ior.right(updated))
 
-  def alwaysIgnore[A, B >: A]: UpdateRule[A, B, A] =
-    UpdateRule.pure[A, B, A]((original, update) => Ior.right(original))
+  def alwaysIgnore[A, B >: A]: UpdateRule[A, B] =
+    UpdateRule.pure[A, B]((original, updated) => Ior.right(original))
 
-  def cannotChange[A, B >: A]: UpdateRule[A, B, B] =
+  def cannotChange[A, B >: A]: UpdateRule[A, B] =
     UpdateRule.cannotChange(errors("Cannot be changed"))
 
-  def cannotChange[A, B >: A](messages: Messages): UpdateRule[A, B, B] =
-    UpdateRule.pure[A, B, B] { (original, update) =>
-      if(original == update) {
-        Ior.right(update)
+  def cannotChange[A, B >: A](messages: Messages): UpdateRule[A, B] =
+    UpdateRule.pure[A, B] { (original, updated) =>
+      if(original == updated) {
+        Ior.right(updated)
       } else {
         Ior.both(messages, original)
       }
@@ -92,13 +89,13 @@ trait PropertyUpdateRules {
   // TODO: Find a nicer syntax for this.
   // We need the empty parens here to avoid having to resolve ambiguity
   // by annotating function parameter types.
-  def canChangeTo[A, B >: A]()(test: B => Boolean): UpdateRule[A, B, B] =
+  def canChangeTo[A, B >: A]()(test: B => Boolean): UpdateRule[A, B] =
     UpdateRule.canChangeTo(errors("Cannot be changed to that value"))(test)
 
-  def canChangeTo[A, B >: A](messages: Messages)(test: B => Boolean): UpdateRule[A, B, B] =
-    UpdateRule.pure[A, B, B] { (original, update) =>
-      if(original == update || test(update)) {
-        Ior.right(update)
+  def canChangeTo[A, B >: A](messages: Messages)(test: B => Boolean): UpdateRule[A, B] =
+    UpdateRule.pure[A, B] { (original, updated) =>
+      if(original == updated || test(updated)) {
+        Ior.right(updated)
       } else {
         Ior.both(messages, original)
       }
@@ -109,18 +106,18 @@ trait PropertyUpdateRules {
 trait UpdateRule1Syntax {
   import scala.language.experimental.macros
 
-  implicit class UpdateRule1Ops[A, B](self: UpdateRule[A, B, A]) {
+  implicit class UpdateRule1Ops[A](self: UpdateRule1[A]) {
     // TODO: Write unit tests. And be prepared. There's a high chance this is all horribly wrong.
-    def field[C](path: Path, lens: Lens[A, C])(implicit rule: UpdateRule[C, B, C]): UpdateRule[A, B, A] =
-      self andThen rule.at(path, lens)
+    def field[B](path: Path, lens: Lens[A, B])(implicit rule: UpdateRule1[B]): UpdateRule1[A] =
+      self zip rule.at(path, lens) map (_._1)
 
     // TODO: Write unit tests. And be prepared. There's a high chance this is all horribly wrong.
     def field[B](accessor: A => B)(implicit rule: UpdateRule1[B]): UpdateRule1[A] =
       macro UpdateRuleMacros.field[A, B]
 
     // TODO: Write unit tests. And be prepared. There's a high chance this is all horribly wrong.
-    def fieldWith[C](path: Path, lens: Lens[A, C])(implicit builder: A => UpdateRule[C, B, C]): UpdateRule[A, B, A] =
-      self andThen UpdateRule.pure((original, update) => builder(original).at(path, lens).apply(original, update))
+    def fieldWith[B](path: Path, lens: Lens[A, B])(implicit builder: A => UpdateRule1[B]): UpdateRule1[A] =
+      self zip UpdateRule.pure((original, updated) => builder(original).at(path, lens).apply(original, updated)) map (_._1)
 
     // TODO: Write unit tests. And be prepared. There's a high chance this is all horribly wrong.
     def fieldWith[B](accessor: A => B)(implicit builder: A => UpdateRule1[B]): UpdateRule1[A] =
--- src/main/scala/checklist/update/package.scala
+++ src/main/scala/checklist/update/package.scala
@@ -1,5 +1,5 @@
 package checklist
 
 package object update {
-  type UpdateRule1[A] = UpdateRule[A, A, A]
+  type UpdateRule1[A] = UpdateRule[A, A]
 }
