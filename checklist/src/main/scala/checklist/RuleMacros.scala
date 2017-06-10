8a468a9fa8cd682c5b8d37ae3ec9b2d1d522abdc
Dave Gurnell <dave@untyped.com>
2017-06-10 16:40:31 +0100

    Support chained accessors (_.foo.bar) in .field macros

--- checklist/src/main/scala/checklist/RuleMacros.scala
+++ checklist/src/main/scala/checklist/RuleMacros.scala
@@ -8,28 +8,38 @@ class RuleMacros(val c: blackbox.Context) {
   import c.universe._
 
   def field[A: c.WeakTypeTag, B: c.WeakTypeTag](accessor: c.Tree)(rule: c.Tree): c.Tree = {
+    val q"($param) => $rhs" = accessor
     val a = weakTypeOf[A]
     val b = weakTypeOf[B]
-    val name = accessorName(accessor)
-    val path = q"""${name.toString} :: _root_.checklist.PNil"""
-    val lens = q"""monocle.macros.GenLens[$a].apply[$b](_.$name)"""
+    val path = accessorPrefix(accessor)
+    val lens = q"""monocle.macros.GenLens[$a].apply[$b]($accessor)"""
     q"${c.prefix}.field($path, $lens)($rule)"
   }
 
   def fieldWith[A: c.WeakTypeTag, B: c.WeakTypeTag](accessor: c.Tree)(builder: c.Tree): c.Tree = {
     val a = weakTypeOf[A]
     val b = weakTypeOf[B]
-    val name = accessorName(accessor)
-    val path = q"""${name.toString} :: _root_.checklist.PNil"""
-    val lens = q"""monocle.macros.GenLens[$a].apply[$b](_.$name)"""
+    val path = accessorPrefix(accessor)
+    val lens = q"""monocle.macros.GenLens[$a].apply[$b]($accessor)"""
     q"${c.prefix}.fieldWith($path, $lens)($builder)"
   }
 
-  private def accessorName(accessor: c.Tree) =
+  private def accessorPrefix(accessor: c.Tree): Tree = {
+    def fail = c.abort(c.enclosingPosition, errorMessage(s"Argument is not an accessor function literal."))
+
+    @scala.annotation.tailrec
+    def unpack(expr: Tree, accum: List[String]): Tree =
+      expr match {
+        case Ident(_)               => accum.foldRight(q"_root_.checklist.PNil" : Tree)((a, b) => q"$a :: $b")
+        case Select(a, TermName(b)) => unpack(a, b :: accum)
+        case _                      => fail
+      }
+
     accessor match {
-      case q"($param) => $obj.$name" => name
-      case other => c.abort(c.enclosingPosition, errorMessage(s"Argument is not an accessor function literal."))
+      case q"($param) => $rhs" => unpack(rhs, Nil)
+      case other               => fail
     }
+  }
 
   private def errorMessage(prefix: String) =
     s"""
