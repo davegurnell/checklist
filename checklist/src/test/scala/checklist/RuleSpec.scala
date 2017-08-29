f5e6e53640f53f1031d1c15816d3c38c73912617
Jacob Barber <jacob.barber@stackpath.com>
2017-08-29 13:03:08 -0500

    Add tests to some functions I added

--- checklist/src/test/scala/checklist/RuleSpec.scala
+++ checklist/src/test/scala/checklist/RuleSpec.scala
@@ -185,11 +185,41 @@ class CombinatorRulesSpec extends FreeSpec with Matchers {
     rule(-1) should be(Ior.both(errors("fail"), "-1!"))
   }
 
+  "emap" in {
+    val rule = Rule.gt[Int](0, errors("fail")).emap(x => if(x % 2 == 0) Ior.right(x) else Ior.left(errors("mapFail")))
+
+    rule(-1) should be(Ior.left(errors("fail", "mapFail")))
+    rule(0) should be(Ior.both(errors("fail"), 0))
+    rule(1) should be(Ior.left(errors("mapFail")))
+    rule(2) should be(Ior.right(2))
+  }
+
+  "mapMessages" in {
+    val rule = Rule.gt[Int](0, errors("fail")).mapMessages(_ => errors("mapped"))
+
+    rule(0) should be(Ior.both(errors("mapped"), 0))
+    rule(1) should be(Ior.right(1))
+  }
+
+  "mapEachMessage" in {
+    val rule = Rule.gt[Int](0, errors("fail")).mapEachMessage(_.prefix("foo"))
+
+    rule(0) should be(Ior.both(errors("fail").map(_.prefix("foo")), 0))
+    rule(1) should be(Ior.right(1))
+  }
+
   "contramap" in {
     val rule = Rule.gt[Int](0, errors("fail")).contramap[Int](_ + 1)
     rule(+1) should be(Ior.right(2))
     rule( 0) should be(Ior.right(1))
     rule(-1) should be(Ior.both(errors("fail"), 0))
+  }
+
+  "contramapPath" in {
+    val rule: Rule[Int, Int] = Rule.gt[Int](0, errors("fail")).contramapPath("n", _ + 1)
+    rule(+1) should be(Ior.right(2))
+    rule( 0) should be(Ior.right(1))
+    rule(-1) should be(Ior.both(errors("fail").map(_.prefix("n")), 0))
   }
 
   "flatMap" in {
@@ -370,12 +400,14 @@ class RuleBuilderSpec extends FreeSpec with Matchers with RuleHListSyntax {
   case class RawFoo(positive: Int, potentiallyEmptyList: List[String], untrimmed: String)
   case class ValidatedFoo(positive: Int, nonEmptyList: NonEmptyList[String], trimmed: String)
 
-  val rule =
-    Rule.builder[RawFoo]
-      .check("positive", _.positive)(Rule.gte(0, errors("negative")))
-      .check("potentiallyEmptyList", _.potentiallyEmptyList)(Rule.nonEmptyList(errors("empty")))
-      .check("untrimmed", _.untrimmed)(Rule.trimString)
-      .build[ValidatedFoo]
+
+  "check (with path)" - {
+    val rule =
+      Rule.builder[RawFoo]
+        .check("positive", _.positive)(Rule.gte(0, errors("negative")))
+        .check("potentiallyEmptyList", _.potentiallyEmptyList)(Rule.nonEmptyList(errors("empty")))
+        .check("untrimmed", _.untrimmed)(Rule.trimString)
+        .build[ValidatedFoo]
 
     "soft-failures should yield both" in {
       rule(RawFoo(-10, List("foo"), "bar")) should be(Ior.Both(NonEmptyList.of(ErrorMessage("negative", PField("positive", PNil))), ValidatedFoo(-10, NonEmptyList.of("foo"), "bar")))
@@ -388,4 +420,26 @@ class RuleBuilderSpec extends FreeSpec with Matchers with RuleHListSyntax {
     "hard failures should yield left" in {
       rule(RawFoo(1, List(), "bar")) should be(Ior.Left(NonEmptyList.of(ErrorMessage("empty", PField("potentiallyEmptyList", PNil)))))
     }
+  }
+
+  "check (without path)" - {
+    val rule =
+      Rule.builder[RawFoo]
+        .check(_.positive)(Rule.gte(0, errors("negative")))
+        .check(_.potentiallyEmptyList)(Rule.nonEmptyList(errors("empty")))
+        .check(_.untrimmed)(Rule.trimString)
+        .build[ValidatedFoo]
+
+    "soft-failures should yield both" in {
+      rule(RawFoo(-10, List("foo"), "bar")) should be(Ior.Both(NonEmptyList.of(ErrorMessage("negative", PNil)), ValidatedFoo(-10, NonEmptyList.of("foo"), "bar")))
+    }
+
+    "transformations should occur" in {
+      rule(RawFoo(1, List("foo"), "bar ")) should be(Ior.Right(ValidatedFoo(1, NonEmptyList.of("foo"), "bar")))
+    }
+
+    "hard failures should yield left" in {
+      rule(RawFoo(1, List(), "bar")) should be(Ior.Left(NonEmptyList.of(ErrorMessage("empty", PNil))))
+    }
+  }
 }
