e575a500d53defb69f0ff4a130b749110ac976ef
Jacob Barber <jacob.barber@stackpath.com>
2017-10-23 11:20:33 -0500

    Add tests for checkAndDrop

--- refinement/src/test/scala/checklist/refinement/RuleHListSyntaxSpec.scala
+++ refinement/src/test/scala/checklist/refinement/RuleHListSyntaxSpec.scala
@@ -7,50 +7,99 @@ import cats.data.{NonEmptyList, Ior}
 
 class RuleBuilderSpec extends FreeSpec with Matchers with RuleHListSyntax {
   case class RawFoo(positive: Int, potentiallyEmptyList: List[String], untrimmed: String)
-  case class ValidatedFoo(positive: Int, nonEmptyList: NonEmptyList[String], trimmed: String)
 
 
-  "check (with path)" - {
-    val rule =
-      Rule.builder[RawFoo]
-        .check("positive", _.positive)(Rule.gte(0, errors("negative")))
-        .check("potentiallyEmptyList", _.potentiallyEmptyList)(Rule.nonEmptyList(errors("empty")))
-        .check("untrimmed", _.untrimmed)(Rule.trimString)
-        .build[ValidatedFoo]
+  "check" - {
+    case class ValidatedFoo(positive: Int, nonEmptyList: NonEmptyList[String], trimmed: String)
+    "with path" - {
+      val rule =
+        Rule.builder[RawFoo]
+          .check("positive", _.positive)(Rule.gte(0, errors("negative")))
+          .check("potentiallyEmptyList", _.potentiallyEmptyList)(Rule.nonEmptyList(errors("empty")))
+          .check("untrimmed", _.untrimmed)(Rule.trimString)
+          .build[ValidatedFoo]
 
-    "soft-failures should yield both" in {
-      rule(RawFoo(-10, List("foo"), "bar")) should be(Ior.Both(NonEmptyList.of(ErrorMessage("negative", PField("positive", PNil))), ValidatedFoo(-10, NonEmptyList.of("foo"), "bar")))
+      "soft-failures should yield both" in {
+        rule(RawFoo(-10, List("foo"), "bar")) should be(Ior.Both(NonEmptyList.of(ErrorMessage("negative", PField("positive", PNil))), ValidatedFoo(-10, NonEmptyList.of("foo"), "bar")))
+      }
+
+      "transformations should occur" in {
+        rule(RawFoo(1, List("foo"), "bar ")) should be(Ior.Right(ValidatedFoo(1, NonEmptyList.of("foo"), "bar")))
+      }
+
+      "hard failures should yield left" in {
+        rule(RawFoo(1, List(), "bar")) should be(Ior.Left(NonEmptyList.of(ErrorMessage("empty", PField("potentiallyEmptyList", PNil)))))
+      }
     }
 
-    "transformations should occur" in {
-      rule(RawFoo(1, List("foo"), "bar ")) should be(Ior.Right(ValidatedFoo(1, NonEmptyList.of("foo"), "bar")))
-    }
+    "without path" - {
+      val rule =
+        Rule.builder[RawFoo]
+          .check(_.positive)(Rule.gte(0, errors("negative")))
+          .check(_.potentiallyEmptyList)(Rule.nonEmptyList(errors("empty")))
+          .check(_.untrimmed)(Rule.trimString)
+          .build[ValidatedFoo]
 
-    "hard failures should yield left" in {
-      rule(RawFoo(1, List(), "bar")) should be(Ior.Left(NonEmptyList.of(ErrorMessage("empty", PField("potentiallyEmptyList", PNil)))))
+      "soft-failures should yield both" in {
+        rule(RawFoo(-10, List("foo"), "bar")) should be(Ior.Both(NonEmptyList.of(ErrorMessage("negative", PNil)), ValidatedFoo(-10, NonEmptyList.of("foo"), "bar")))
+      }
+
+      "transformations should occur" in {
+        rule(RawFoo(1, List("foo"), "bar ")) should be(Ior.Right(ValidatedFoo(1, NonEmptyList.of("foo"), "bar")))
+      }
+
+      "hard failures should yield left" in {
+        rule(RawFoo(1, List(), "bar")) should be(Ior.Left(NonEmptyList.of(ErrorMessage("empty", PNil))))
+      }
     }
   }
 
-  "check (without path)" - {
-    val rule =
-      Rule.builder[RawFoo]
-        .check(_.positive)(Rule.gte(0, errors("negative")))
-        .check(_.potentiallyEmptyList)(Rule.nonEmptyList(errors("empty")))
-        .check(_.untrimmed)(Rule.trimString)
-        .build[ValidatedFoo]
 
-    "soft-failures should yield both" in {
-      rule(RawFoo(-10, List("foo"), "bar")) should be(Ior.Both(NonEmptyList.of(ErrorMessage("negative", PNil)), ValidatedFoo(-10, NonEmptyList.of("foo"), "bar")))
+  "checkAndDrop" - {
+    case class ValidatedFoo(nonEmptyList: NonEmptyList[String], trimmed: String)
+    "with path" - {
+      val rule =
+        Rule.builder[RawFoo]
+          .checkAndDrop("positive", _.positive)(Rule.gte(0, errors("negative")))
+          .check("potentiallyEmptyList", _.potentiallyEmptyList)(Rule.nonEmptyList(errors("empty")))
+          .check("untrimmed", _.untrimmed)(Rule.trimString)
+          .build[ValidatedFoo]
+
+      "soft-failures should yield both" in {
+        rule(RawFoo(-10, List("foo"), "bar")) should be(Ior.Both(NonEmptyList.of(ErrorMessage("negative", PField("positive", PNil))), ValidatedFoo(NonEmptyList.of("foo"), "bar")))
+      }
+
+      "transformations should occur" in {
+        rule(RawFoo(1, List("foo"), "bar ")) should be(Ior.Right(ValidatedFoo(NonEmptyList.of("foo"), "bar")))
+      }
+
+      "hard failures should yield left" in {
+        rule(RawFoo(1, List(), "bar")) should be(Ior.Left(NonEmptyList.of(ErrorMessage("empty", PField("potentiallyEmptyList", PNil)))))
+      }
     }
 
-    "transformations should occur" in {
-      rule(RawFoo(1, List("foo"), "bar ")) should be(Ior.Right(ValidatedFoo(1, NonEmptyList.of("foo"), "bar")))
-    }
+    "without path" - {
+      val rule =
+        Rule.builder[RawFoo]
+          .checkAndDrop(_.positive)(Rule.gte(0, errors("negative")))
+          .check(_.potentiallyEmptyList)(Rule.nonEmptyList(errors("empty")))
+          .check(_.untrimmed)(Rule.trimString)
+          .build[ValidatedFoo]
 
-    "hard failures should yield left" in {
-      rule(RawFoo(1, List(), "bar")) should be(Ior.Left(NonEmptyList.of(ErrorMessage("empty", PNil))))
+      "soft-failures should yield both" in {
+        rule(RawFoo(-10, List("foo"), "bar")) should be(Ior.Both(NonEmptyList.of(ErrorMessage("negative", PNil)), ValidatedFoo(NonEmptyList.of("foo"), "bar")))
+      }
+
+      "transformations should occur" in {
+        rule(RawFoo(1, List("foo"), "bar ")) should be(Ior.Right(ValidatedFoo(NonEmptyList.of("foo"), "bar")))
+      }
+
+      "hard failures should yield left" in {
+        rule(RawFoo(1, List(), "bar")) should be(Ior.Left(NonEmptyList.of(ErrorMessage("empty", PNil))))
+      }
     }
   }
+
 
   "append" - {
     case class Foo(n: Int)
